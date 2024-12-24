d92e570584b113c855d770ec90dd47c3e758bf3f
IGor <igorek.lesnoi30@gmail.com>
2024-12-24 12:48:02 +0200

    Implement random computer move

--- src/Director/tTicTacToe/component/ComputerMove.java
+++ src/Director/tTicTacToe/component/ComputerMove.java
@@ -16,10 +16,22 @@
 
 package Director.tTicTacToe.component;
 
+import Director.tTicTacToe.model.Cell;
 import Director.tTicTacToe.model.GameTable;
+
+import java.util.Random;
 
 public class ComputerMove {
     public void make(final GameTable gameTable) {
-
+        final Random random = new Random();
+        while (true) {
+            final int row = random.nextInt(3);
+            final int col = random.nextInt(3);
+            final Cell randomCell = new Cell(row, col);
+            if (gameTable.isEmpty(randomCell)) {
+                gameTable.setSign(randomCell, '0');
+                return;
+            }
+        }
     }
 }
